44b2d68d058176724239eae40bcae7ca8a53a174
Cruz-D <dcgord1997@gmail.com>
2024-10-15 22:35:35 +0200

    Agregar archivos de proyecto.

---
+++ examen15102024/JavaScript.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+
+    /*******************************/
+    /*Script 1*/
+    $("p").addClass("borde");
+
+    /*******************************/
+    /*Script 2*/
+    $("li .azul").removeClass("azul").addClass("verde");
+
+    /*******************************/
+    /*Script 3*/
+    $("p").removeClass("borde");
+
+    /*******************************/
+    /*Script 4*/
+    $("p").on('click', function () {
+
+        if (!$(this).hasClass("borde")) {
+
+            $(this).addClass("borde");
+
+        } else {
+
+            $(this).removeClass("borde");
+
+        }
+
+    });
+
+    /*******************************/
+    /*Script 5*/
+    $("ul li:not([data-hours])").selectable();
+
+    /*******************************/
+    /*Script 6*/
+    $("li:empty").text("Programacion en Shell");
+
+    /*******************************/
+    /*Script 7*/
+
+    $("p:primero").text("modulos de primero");
+
+});
